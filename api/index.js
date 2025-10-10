831c346c1ead1423b08c8e0b0b29be0145635891
Deekshith-46 <tdeekshith46@gmail.com>
2025-10-10 14:31:42 +0530

    vercel updates

--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
-const serverless = require('serverless-http');
 const app = require('../src/app');
 
-// create the handler once to avoid re-instantiation per request
-const handler = serverless(app);
-
-module.exports = handler;
+// Export the Express app directly as the request handler
+module.exports = app;
 
 
